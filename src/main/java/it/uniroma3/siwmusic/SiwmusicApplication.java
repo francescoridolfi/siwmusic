30bc2ea7ce5c43ab1b6971e0e0dafd731683bf4f
Francesco Ridolfi <francesco.ridolfi.02@gmail.com>
2025-07-15 23:16:40 +0200

    Inizializzato il progetto e configurazione di base

---
+++ src/main/java/it/uniroma3/siwmusic/SiwmusicApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siwmusic/SiwmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
